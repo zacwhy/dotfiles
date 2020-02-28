4e015973067277b0b4916162b361a5f455dd4bbc
zy <zacwhy@gmail.com>
2020-02-28 17:17:51 +0800

    Modify alias for git

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -4,5 +4,14 @@ alias ls='ls -FG'
 alias l='ls -1A'
 alias ll='ls -Alh'
 
-alias g='git'
 alias rm='rm -i'
+
+# No arguments: `git status`
+# With arguments: acts like `git`
+g() {
+  if [[ $# -gt 0 ]]; then
+    git "$@"
+  else
+    git status
+  fi
+}
