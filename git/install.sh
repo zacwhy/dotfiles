ad5b406e75f875fa9fd2a3496789fa579a1c3b8a
zy <zacwhy@gmail.com>
2020-03-02 10:39:28 +0800

    Add .gitignore

--- git/install.sh
+++ git/install.sh
@@ -2,7 +2,10 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-echo ln -s ${SCRIPTPATH}/.gitconfig ${HOME}/.gitconfig
+echo Executing: ln -s ${SCRIPTPATH}/.gitconfig ${HOME}/.gitconfig
 #ln -s ${SCRIPTPATH}/.gitconfig ${HOME}/.gitconfig
 
-echo done
+echo Executing: ln -s ${SCRIPTPATH}/.gitignore ${HOME}/.gitignore
+#ln -s ${SCRIPTPATH}/.gitignore ${HOME}/.gitignore
+
+echo Done
